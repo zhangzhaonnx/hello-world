d8de7ac63ab63ab490f2a8efb760bc0293ea4e73
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-11-26 15:08:59 +0800

    add test

---
+++ src/test/java/com/example/helloworld/HelloControllerTest.java
@@ -0,0 +1,22 @@
+package com.example.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void hello() throws Exception {
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk());
+    }
+}
